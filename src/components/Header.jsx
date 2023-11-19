44e837ea8f344a3cce9854aa98451a6fe4d6b7a2
julieMdn <julie.midon@gmail.com>
2023-11-19 15:57:15 +0100

    import logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import "./header.css";
 import { Link } from "react-router-dom";
+import logoMarvel from "../assets/pictures/logo-marvel.png";
 
 const Header = () => {
   return (
     <div className="header-container flux">
-      <img src="./src/assets/pictures/logo-marvel.png" alt="logo-marvel" />
+      <img src={logoMarvel} alt="logo-marvel" />
       <ul>
         <Link to="/">
           <li>Personnages</li>
